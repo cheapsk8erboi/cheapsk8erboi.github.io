81913b00b6eba9d4ab1540d0f6f38558b1c19e8e
Artifex <64751745+cheapsk8erboi@users.noreply.github.com>
2025-02-23 20:07:40 -0500

    rest batch

---
+++ prices.js
@@ -0,0 +1,64 @@
+//Minimums
+$(function(){
+		$('.MonthlyMinimumWage').text('$1,218');
+	});
+$(function(){
+		$('.QOLCost').text('$4,174.94');
+	});	
+$(function(){
+		$('.HourlyQOLPay').text('$24.09');
+	});		
+//Vital Necessities
+	$(function(){
+		$('.VitalPrice').text('$465.44');// figure out how to combine food & water automatically to further minimize workload//
+	});
+
+		$(function(){
+			$('.WaterPrice').text('$47');
+		});
+
+		$(function(){
+			$('.FoodPrice').text('$418.44');
+		});
+//Critical Necessities
+	$(function(){
+		$('.CriticalPrice').text('$2,347');
+	});
+
+		$(function(){
+			$('.ElectricityPrice').text('$130');
+		});
+
+		$(function(){
+			$('.ShelterPrice').text('$2,120');
+		});	
+		$(function(){
+			$('.NaturalGasPrice').text('$97');
+		});	
+//Quality Necessities
+	$(function(){
+		$('.QualityPrice').text('$659');
+	});
+
+		$(function(){
+			$('.HealthcarePrice').text('$447');
+		});
+
+		$(function(){
+			$('.ClothingPrice').text('$120');
+		});	
+		$(function(){
+			$('.EducationPrice').text('$92');
+		});	
+//Quality Necessities
+	$(function(){
+		$('.StructuralPrice').text('$703.05');
+	});
+
+		$(function(){
+			$('.MotorVehiclePrice').text('$628.05');
+		});
+
+		$(function(){
+			$('.InternetPrice').text('$75');
+		});	
